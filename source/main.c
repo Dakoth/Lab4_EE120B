0cf1831619b276a74b67cb73bda6611dd3c30a17
Alfredo Gonzalez <agonz250@wch136-11.cs.ucr.edu>
2020-04-14 19:49:34 -0700

    editing main for part 1

--- source/main.c
+++ source/main.c
@@ -12,6 +12,56 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States {Start, Arelease, Bhold, Brelease, Ahold} state; 
+
+void Tick() {
+	switch(Start) { //transitions
+		case Start: //beginnning state
+			state = Arelease;
+			break;
+		
+		case Arelease: //
+			if (tmpA == 0x01) {
+				state = Bhold;	
+			} 	
+			else {
+				state = Arelease; 
+			}
+			break;
+
+		case Bhold: 
+			if (tmpA == 0x01) {
+				state = Bhold;
+			}	
+			else {
+				state = Brelease;
+			}
+			break;
+		
+		case Brelease: 
+			if (tmpA == 0x01) {
+				state = Ahold;
+			}
+			else { state = Brelease; }
+			break;
+
+		case Ahold:
+			if (tmpA == 0x01) { state = Ahold;}
+			else { state = Arelease; }
+			break; 
+
+		default: 
+			//shouldn't go here
+			break;					
+	}
+
+
+
+
+
+
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF; //Makes all As as input
@@ -21,7 +71,11 @@ int main(void) {
 	unsigned char tmpB; 
 	/* Insert your solution below */
     	while (1) {
+		tmpA = PORTA; 
 
+
+
+		PORTA = tmpB;		
     	}
     	return 1;
 }
